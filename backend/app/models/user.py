add26226d68ee346447c6390b9323276684235eb
KaushakiB <kaushakibilgrami@gmail.com>
2026-02-13 13:02:42 +0530

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,1 +1,7 @@
+# models/user.py   ← at least do this for now
+from pydantic import BaseModel
 
+class User(BaseModel):
+    name: str
+    email: str
+    # you can add more later: subjects: list[str], year: int, etc.
