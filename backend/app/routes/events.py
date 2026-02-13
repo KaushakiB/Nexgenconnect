16310d44b23c49ed34ccabf8acfff632293a3877
KaushakiB <kaushakibilgrami@gmail.com>
2026-02-13 12:01:46 +0530

    Update events.py

--- backend/app/routes/events.py
+++ backend/app/routes/events.py
@@ -1,1 +1,10 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class Event(BaseModel):
+    title: str
+    city: str
+    destination: str
+    travel_date: datetime
+    transport_mode: str
 
